6dbbeea9e3d499c25da5a1aeba3911f01ecbc7f8
xyder <xydr2311@gmail.com>
2020-11-15 14:12:18 +0000

    Added installer script

---
+++ bin/installer_apple.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+
+# package id for v0.2.3
+pkg_id=4638918
+
+curl https://gitlab.com/xyder/nxcmdr/-/package_files/$pkg_id/download --output /tmp/nxc.tar.gz
+
+dest_dir=$HOME/.local/bin
+
+mkdir -p $dest_dir
+
+tar -xf /tmp/nxc.tar.gz -C $dest_dir ./nxc
+
+rm /tmp/nxc.tar.gz
